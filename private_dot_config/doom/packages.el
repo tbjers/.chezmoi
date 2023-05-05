b4c193e39ce7c845f266d1a7d32e9546501e98a5
Torgny Bjers <torgny@bjers.org>
2023-05-05 00:15:29 -0400

    config update

--- private_dot_config/doom/packages.el
+++ private_dot_config/doom/packages.el
@@ -26,6 +26,7 @@
 (package! flycheck-yamllint)
 (package! yaml-mode)
 (package! lsp-pyright)
+(package! terraform-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
